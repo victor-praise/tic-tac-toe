40d393fe975a9731e83784af74473b9c3e2cc2cf
victor-praise <61267951+victor-praise@users.noreply.github.com>
2025-07-23 22:52:45 -0400

    react tic tac toe complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cell from "./components/Cell";
 import "../src/index.css";
 
@@ -7,13 +7,42 @@ function App() {
   const [go, setGo] = useState("circle");
   const message = "it is now " + go + "'s turn";
   const [winningMessage, setWinningMessage] = useState("");
+  const checkScore = () => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    winningCombinations.forEach((combination) => {
+      let circleWins = combination.every(cell => cells[cell] === "circle");
+      let crossWins = combination.every(cell => cells[cell] === "cross");
+      if (circleWins) {
+        setWinningMessage("Player circle wins!");
+        return
+      }
+      if (crossWins) {
+        setWinningMessage("Player cross wins!");
+        return;
+      }
+    })
+
+  };
+  useEffect(() => {
+    checkScore();
+  }, [cells]);
   return (
     
     <div className="App">
     <div className="gameboard">
      
       {cells.map((cell, index) => (
-        <Cell key={index} cell={cell} id={index} />
+        <Cell key={index} cell={cell} id={index} go={go} setGo={setGo} cells={cells} setCells={setCells} winningMessage={winningMessage} />
       ))}
     </div>
     <p> {winningMessage || message} </p>
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,40 @@
-const Cell = ({cell, id}) => {
-    const handleClick = () => {
+const Cell = ({cell, id,go,setGo,cells,setCells,winningMessage}) => {
+  
+    const handleClick = (e) => {
         // Logic to handle cell click, e.g., updating state or checking for a win
-        console.log(`Cell ${id} clicked with value: ${cell}`);
+       
+        if(!winningMessage){
+           const taken = e.target.firstChild?.classList.contains("circle") || e.target.firstChild?.classList.contains("cross") || e.target.classList.contains("circle") || e.target.classList.contains("cross");
+  
+        if (!taken) {
+          if(go === "circle") {
+            e.target.firstChild.classList.add("circle");
+            handleCellChange("circle");
+          setGo("cross");
+          } else if (go === "cross") {
+            e.target.firstChild.classList.add("cross");
+            handleCellChange("cross");
+            setGo("circle");
+          
+          }
+        }
+        }
+       
+    };
+    const handleCellChange = (className) => {
+        // Logic to handle cell change, if needed
+      const nextCell = cells.map((cell,index)=>{
+          if(index === id) {
+            return className;
+          } else {
+            return cell;
+          }
+        })
+        setCells(nextCell);
     };
   return (
   <div className="square" id={id} onClick={handleClick}>
-    <div> {cell}</div>
+    <div> </div>
   </div>
 
   );
